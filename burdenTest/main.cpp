0fae39f41561e5174551630d632da85bd6ec5b15
Corin Thummel <thummelcorin@gmail.com>
2018-06-29 10:51:25 -0600

    Initial Commit

---
+++ burdenTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  burdenTest
+//
+//  Created by Corin Thummel on 6/29/18.
+//  Copyright © 2018 Corin Thummel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
