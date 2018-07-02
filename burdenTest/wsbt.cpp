12b77c13168ceee7675bcbb10201537c207e31ae
Corin Thummel <thummelcorin@gmail.com>
2018-07-02 09:33:17 -0600

    initial commit

---
+++ burdenTest/wsbt.cpp
@@ -0,0 +1,9 @@
+//
+//  wsbt.cpp
+//  burdenTest
+//
+//  Created by Corin Thummel on 6/29/18.
+//  Copyright © 2018 Corin Thummel. All rights reserved.
+//
+
+#include "wsbt.hpp"
---
+++ burdenTest/wsbt.hpp
@@ -0,0 +1,14 @@
+//
+//  wsbt.hpp
+//  burdenTest
+//
+//  Created by Corin Thummel on 6/29/18.
+//  Copyright © 2018 Corin Thummel. All rights reserved.
+//
+
+#ifndef wsbt_hpp
+#define wsbt_hpp
+
+#include <stdio.h>
+
+#endif /* wsbt_hpp */
